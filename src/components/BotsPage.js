8b756e9c8961428246431f28ce6be99329bce679
Steve <steve.maina@student.moringaschool.com>
2022-09-30 18:16:05 +0300

    first commit

---
+++ src/components/BotsPage.js
@@ -0,0 +1,50 @@
+import React, { useState , useEffect } from "react";
+import YourBotArmy from "./YourBotArmy";
+import BotCollection from "./BotCollection";
+
+const API = "http://localhost:8002/bots";
+
+function BotsPage() {
+  //start here with your code for step one
+
+  const [bots, setBots] = useState([]);
+  
+  useEffect(() => {
+    fetch(API)
+    .then(res => res.json())
+    // .then(json => console.log(json))
+    .then(setBots)
+  },[])
+
+  function botList(bot){
+    console.log(bot);
+    setBots(bots.map(b => b.id === bot.id ? {...b, army:true} : b));
+  }
+
+  function removeBot(bot){
+    console.log(bot);
+    setBots(bots.map(b => b.id === bot.id ? {...b, army:false} : b));
+  }
+
+  function BotDelete(bot){
+    // console.log("You're fired.")
+    setBots(bots.filter(b => b.id !== bot.id))
+  }
+  return (
+    <div>
+      <YourBotArmy 
+      bots={bots.filter(b => b.army)}
+      removeBot ={removeBot}
+      BotDelete={BotDelete}
+      />
+
+      <BotCollection 
+      bots={bots}
+      botList={botList}
+      deleteBot={BotDelete}
+      />
+    </div>
+  )
+}
+
+export default BotsPage;
